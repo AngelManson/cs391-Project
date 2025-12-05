a71e53dc5ff6fbf904a7a8c3c9a2b85f714c1b0b
AngelManson <aom@bu.edu>
2025-12-05 00:13:56 -0500

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 export default function Footer() {
     return (
-        <footer className="flex items-center justify-center"> All Rights Reserved <Link href={'/about'}> Credits</Link> &#169;</footer>
+        <footer className="flex items-center justify-center">
+            All Rights Reserved <Link href={'/about'} className={`font-bold text-blue-900 !ml-1 !mr-1`}> Credits </Link> &#169;
+        </footer>
     );
 }
